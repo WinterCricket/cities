7469dd9f7ef3d0dfa368a9fb862f1944302ee185
WinterCricket <hodhodhod2003@gmail.com>
2017-07-29 23:07:41 -0500

    refactor and reindent

--- cityPollution.js
+++ cityPollution.js
@@ -1,16 +1,16 @@
 function CleanCities() {
-var cleanestCities = ["Boston", "Quebec", "Paris", "Vienna"];
-var numElements = cleanestCities.length;
-var matchFound = "no";
-var cityToCheck = prompt("Which city do you want to check?");
-for (var i = 0; i < numElements; i++) {
-  if (cityToCheck === cleanestCities[i]) {
-    matchFound = "yes";
-    alert(cityToCheck + " is a damned clean city!");
-    break;
-  }
-}
-if (matchFound === "no") {
-  alert(cityToCheck + " is not especially clean for a city.");
-}
+	var cleanestCities = ["Boston", "Quebec", "Paris", "Vienna"];
+	var numElements = cleanestCities.length;
+	var matchFound = "no";
+	var cityToCheck = prompt("Which city do you want to check?");
+	for (var i = 0; i < numElements; i++) {
+		if (cityToCheck === cleanestCities[i]) {
+			matchFound = "yes";
+			alert(cityToCheck + " is a damned clean city!");
+			break;
+		}
+	}
+	if (matchFound === "no") {
+		alert(cityToCheck + " is not especially clean for a city.");
+	}
 }
