e5bf76bc0d9a506f1473127058a130c22d90a8bd
WinterCricket <hodhodhod2003@gmail.com>
2017-07-29 23:17:18 -0500

    add boolean

--- cityPollution.js
+++ cityPollution.js
@@ -1,11 +1,11 @@
 function CleanCities() {
 	var cleanestCities = ["Boston", "Quebec", "Paris", "Vienna"];
 	var numElements = cleanestCities.length;
-	var matchFound = "no";
+	var matchFound = false;
 	var cityToCheck = prompt("Which city do you want to check?");
 	for (var i = 0; i < numElements; i++) {
 		if (cityToCheck === cleanestCities[i]) {
-			matchFound = "yes";
+			matchFound = true;
 			alert(cityToCheck + " is a damned clean city!");
 			break;
 		}
