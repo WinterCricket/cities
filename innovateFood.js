f1fdca02388cc4c5394e328729e2d1e4afa18dc4
WinterCricket <hodhodhod2003@gmail.com>
2017-07-30 21:27:23 -0500

    Intro k counter

--- innovateFood.js
+++ innovateFood.js
@@ -1,7 +1,10 @@
 var animals = ["cat", "dog"];
 var foods = ["milk", "cheese", "eggs"];
+var foodItems = [];
+var k = 0;
 for (var i = 0; i < animals.length; i++) {
-  for (var j = 0; j < foods.length; j++) {
-    console.log(weirdFoods = animals[i] + foods[j]);
-  }
+	for (var j = 0; j < foods.length; j++) {
+		alert(foodItems[k] = animals[i] + foods[j]);
+		k++;
+	}
 }
