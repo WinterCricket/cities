766b62ecf21985ef51c9b201aa5726896d0c7eec
WinterCricket <hodhodhod2003@gmail.com>
2017-08-01 16:06:41 -0500

    replace with counted numbers with variables

--- bannedSeg.js
+++ bannedSeg.js
@@ -1,8 +1,9 @@
 var text = "It wasn't nice, not one bit a good thing, to have slept on a lumpy cushion, with snow falling outside, and bombs falling, too, for it was World War II, though it wasn't called that back then. Mare didn't call it anything but an interuption, or, as she said, a pain in the arse.";
 var term = "World War II";
+var replacement = "the Second World War";
 var firstChar = text.indexOf(term);
 if (firstChar !== -1) {
-  text = text.slice(0, firstChar) + " the Second World War " + text.slice(firstChar + 12);
+  text = text.slice(0, firstChar) + replacement + text.slice(firstChar + term.length);
   alert(text);
 }
 else {
