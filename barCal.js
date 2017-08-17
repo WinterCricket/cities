a58197ea403da2ec44fa797eae7e002396f14922
WinterCricket <hodhodhod2003@gmail.com>
2017-08-17 17:40:52 -0500

    Switch to switch statements

--- barCal.js
+++ barCal.js
@@ -1,15 +1,20 @@
 var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 var dayNum = new Date().getDay();
 var dayOfWk = days[dayNum];
-if (dayOfWk === "Sat" || dayOfWk === "Sun") {
-  alert("Happy Days!");
-}
-else if (dayOfWk === "Fri") {
-  alert("Here we go, two days of drinking.")
-}
-else if (dayOfWk === "Thu") {
+
+switch(dayOfWk) {
+case "Sat" :
+  alert("Ensure you've got a hangover!");
+  break;
+case "Sun" :
+  alert("Recover from you drink!");
+  break;
+case "Fri" :
+  alert("Second Day of Drinking.");
+  break;
+case "Thu" :
   alert("Thirsty Thursday!");
-}
-else {
+  break;
+default :
   alert("Go back to bed!");
 }
