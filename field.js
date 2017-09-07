d9be9e0a5d8df9175e249d01165e1bdeb9b0717d
WinterCricket <hodhodhod2003@gmail.com>
2017-09-07 03:18:03 -0500

    little app exploring fields

---
+++ field.js
@@ -0,0 +1,8 @@
+function checkAddress(fieldId) {
+  if (document.getElementById(fieldId).value === "") {
+    alert("Email address required");
+  }
+  else {
+  	alert("You have entered " + val);
+  }
+}
