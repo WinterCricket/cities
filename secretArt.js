c476685898a6356b7299c85f36c8a01d01a89500
WinterCricket <hodhodhod2003@gmail.com>
2017-09-10 23:33:15 -0500

    add the expanding graph feature

---
+++ secretArt.js
@@ -0,0 +1,5 @@
+function longGraph() {
+	var fullGraph = "To summarize briefly, the emotions--especially basic emotions--are commonly held to be biologically primitive responses, hard-wired into the nervous system over the course of evolution. But who cares at this stage? Rhetoric and emotion are the bald pups of Rome lore. If this makes sense to you, your are either highly imaginative or insane, though you could be both. You see, this is filler until I put real content into my app.";
+	document.getElementById("secretArt").innerHTML = fullGraph;
+
+}
