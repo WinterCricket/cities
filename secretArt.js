4972d459ca154730f4cd21408e6e35bde1874770
WinterCricket <hodhodhod2003@gmail.com>
2017-09-11 02:17:26 -0500

    incorporate ol into graph extender

--- secretArt.js
+++ secretArt.js
@@ -1,5 +1,5 @@
 function longGraph() {
-	var fullGraph = "To summarize briefly, the emotions--especially basic emotions--are commonly held to be biologically primitive responses, hard-wired into the nervous system over the course of evolution. But who cares at this stage? Rhetoric and emotion are the bald pups of Rome lore. If this makes sense to you, your are either highly imaginative or insane, though you could be both. You see, this is filler until I put real content into my app.";
+	var fullGraph = "To summarize briefly,<ol><li> the emotions--especially basic emotions--are commonly held to be biologically primitive responses,</li><li> hard-wired into the nervous system </li><li>over the course of evolution. But who cares at this stage?</li><li> Rhetoric and emotion are the bald pups of Rome lore.</li><li> If this makes sense to you,</li><li> you are either highly imaginative or insane, </li><li>though you could be both.</li><li> You see, this is filler until I put real content into my app.</li>";
 	document.getElementById("secretArt").innerHTML = fullGraph;
 
 }
