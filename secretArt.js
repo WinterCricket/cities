353d6c71b662b05629121ff1b15cb9a7e84fd170
WinterCricket <hodhodhod2003@gmail.com>
2017-09-16 16:56:06 -0500

    add list element to extension

--- secretArt.js
+++ secretArt.js
@@ -1,5 +1,5 @@
 function longGraph() {
-	var fullGraph = "To summarize briefly,<ol><li> the emotions--especially basic emotions--are commonly held to be biologically primitive responses,</li><li> hard-wired into the nervous system </li><li>over the course of evolution. But who cares at this stage?</li><li> Rhetoric and emotion are the bald pups of Rome lore.</li><li> If this makes sense to you,</li><li> you are either highly imaginative or insane, </li><li>though you could be both.</li><li> You see, this is filler until I put real content into my app.</li>";
+	var fullGraph = "If civilization is prolonged childhood, a child whose goal is safety, then the arts, if not the means of civilization, contains its meaning. To summarize briefly,<ol><li> the emotions--especially basic emotions--are commonly held to be biologically primitive responses,</li><li> hard-wired into the nervous system </li><li>over the course of evolution. But who cares at this stage?</li><li> Rhetoric and emotion are the bald pups of Rome lore.</li><li> If this makes sense to you,</li><li> you are either highly imaginative or insane, </li><li>though you could be both.</li><li> You see, this is filler until I put real content into my app.</li>";
 	document.getElementById("secretArt").innerHTML = fullGraph;
 
 }
